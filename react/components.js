3bc1c4ff85bef87f1cf08c9c45c4ed68da494e98
cyd losekann <cydnel@gmail.com>
2022-09-28 05:09:41 -0300

    components & composition added

---
+++ react/components.js
@@ -0,0 +1,11 @@
+// Function Component
+const Button = () => {
+  return <button>Comprar</button>;
+};
+
+// Class Component
+class Button extends React.Component {
+  render() {
+    return <button>Comprar</button>;
+  }
+}
