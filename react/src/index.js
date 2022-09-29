8914d254d881b2b1388e86cd8501277cd90c2328
cyd losekann <cydnel@gmail.com>
2022-09-29 03:49:27 -0300

    index.js ok

--- react/src/index.js
+++ react/src/index.js
@@ -1,1 +1,5 @@
-console.log('teste');
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
