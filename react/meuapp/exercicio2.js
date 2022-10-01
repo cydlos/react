b5f3d481711b4c3400c1b9334343956f5e4ff777
cyd losekann <cydnel@gmail.com>
2022-10-01 16:15:56 -0300

    both exercises done

--- react/meuapp/exercicio2.js
+++ react/meuapp/exercicio2.js
@@ -1,3 +1,4 @@
+
 // Mostre os dados da aplicação, como aprensetado no vídeo
 // Não utilize CSS externo, use o style para mudar as cores
 // Se a situação estiver ativa pinte de verde, inativa vermelho
