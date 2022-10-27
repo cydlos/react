d479aebc4393cd1f3fe44d119722a92b57bb1e34
cyd losekann <cydnel@gmail.com>
2022-10-27 14:38:34 -0300

    .

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -1,6 +1,6 @@
 // Within the src directory, along with the index.js file, create a new file called App.js.
 // This will be the main component for our application.
-// You can use the components found in the "components" directory to build out the App component.
+// You can use the components found in the "components" directory to build out the App component
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
